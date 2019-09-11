6409f553f808ac53005b8156c5b745c097337ae8
Yevgeniy <zhe.tovtin@gmail.com>
2019-09-10 21:00:26 -0700

    started esercices

---
+++ goit-js-hw-01/task-02.js
@@ -0,0 +1,17 @@
+const total = 100;
+const ordered = 50;
+
+if (ordered > total) {
+  console.log('На складе недостаточно твоаров!');
+} else {
+  console.log('Заказ оформлен, с вами свяжется менеджер');
+}
+
+const totalNew = 100;
+const orderedNew = 150;
+
+if (orderedNew > totalNew) {
+  console.log('На складе недостаточно твоаров!');
+} else {
+  console.log('Заказ оформлен, с вами свяжется менеджер');
+}
