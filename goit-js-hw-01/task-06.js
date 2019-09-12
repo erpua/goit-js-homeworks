e55152b89e693e9ba296bcad966ce859b73e0d18
Yevgeniy <zhe.tovtin@gmail.com>
2019-09-11 21:16:09 -0700

    finished last exercise

--- goit-js-hw-01/task-06.js
+++ goit-js-hw-01/task-06.js
@@ -1,4 +1,4 @@
-const numbers = [];
+/* const numbers = [];
 let input;
 let total = 0;
 
@@ -6,9 +6,48 @@ do {
   input = prompt('Введите число');
   if (input === null || input === '' || isNaN(input)) break;
   numbers.push(+input);
-} while (true);
+} while (input);
 
 for (let i = 0; i < numbers.length; i += 1) {
   total += numbers[i];
 }
-alert(`Cумма всех значений в массиве ${total}`);
+alert(`Cумма всех значений в массиве ${total}`); */
+
+const numbers = [];
+let input;
+let total = 0;
+
+do {
+  input = prompt('Введите число');
+  const inputNumber = +input;
+  if (input === null) break;
+  if (input === '') break;
+  if (Number.isNaN(inputNumber)) break;
+  numbers.push(inputNumber);
+} while (input);
+
+for (let i = 0; i < numbers.length; i += 1) {
+  total += numbers[i];
+}
+alert(`Сумма всех чисел в массиве ${total}`);
+
+/* let userInput;
+let total = 0;
+
+while (true) {
+  userInput = prompt('Введите число больше нуля');
+
+  if (userInput === null) {
+    break;
+  }
+
+  userInput = Number(userInput);
+
+  if (userInput <= 0) {
+    break;
+  }
+
+  total += userInput;
+}
+
+console.log(total); */
