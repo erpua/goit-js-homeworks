ec82a0faa00409bf013d6c2a7164373fbc4d3594
Tovtin <intn@MXPC010033.local>
2019-11-01 08:54:08 -0600

    added changes

--- goit-js-hw-07/task-06.js
+++ goit-js-hw-07/task-06.js
@@ -2,8 +2,16 @@ const input = document.querySelector('#validation-input');
 
 const handleInputBlur = e => {
   e.preventDefault();
-  if (input.value.length == input.dataset.length) input.classList.add('valid');
-  else input.classList.add('invalid');
+  if (
+    input.dataset.length < input.value.length ||
+    input.dataset.length > input.value.length
+  ) {
+    input.classList.remove('valid');
+    input.classList.add('invalid');
+  } else {
+    input.classList.remove('invalid');
+    input.classList.add('valid');
+  }
 };
 
 input.onblur = handleInputBlur;
