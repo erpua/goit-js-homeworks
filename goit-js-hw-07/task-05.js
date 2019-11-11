2f0360396d2d9cb5a343cfe37211c0309ffa33bf
Tovtin <intn@MXPC010033.local>
2019-11-11 08:00:20 -0600

    changed module 7

--- goit-js-hw-07/task-05.js
+++ goit-js-hw-07/task-05.js
@@ -6,6 +6,7 @@ const refs = {
 function handleInput(e) {
   e.preventDefault();
   refs.name.textContent = e.target.value;
+  if (refs.input.value.length == 0) refs.name.textContent = 'незнакомец';
 }
 
 refs.input.addEventListener('input', handleInput);
