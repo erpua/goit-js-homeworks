64fea281f596fbad0854382fc4e31e89449f33aa
Yevgeniy Tovtin <zhe.tovtin@gmail.com>
2019-11-21 23:12:38 -0600

    added changes module_10

--- goit-js-hw-10-food-service/src/app.js
+++ goit-js-hw-10-food-service/src/app.js
@@ -1,1 +1,5 @@
 import refreshList from './components/menu';
+import data from './db/menu.json';
+import refs from './utils/refs';
+
+refreshList(data, refs.menu);
--- goit-js-hw-10-food-service/src/components/menu.js
+++ goit-js-hw-10-food-service/src/components/menu.js
@@ -1,5 +1,7 @@
+import template from '../templates/menu-items.hbs';
+
 function refreshList(data, root) {
-  const html = data.map(e => `<li>${e}</li>`);
+  const html = data.map(e => template(e)).join('\n');
   root.insertAdjacentHTML('afterbegin', html);
 }
 
