024af55bf5b397b5790288b370821ef9a6613e70
Yevgeniy Tovtin <zhe.tovtin@gmail.com>
2019-11-23 10:29:39 -0600

    added changes module_10

--- goit-js-hw-10-food-service/src/app.js
+++ goit-js-hw-10-food-service/src/app.js
@@ -1,5 +1,10 @@
 import refreshList from './components/menu';
+import changeTheme from './components/theme';
 import data from './db/menu.json';
 import refs from './utils/refs';
 
+const checkBox = document.querySelector('#theme-switch-control');
+
 refreshList(data, refs.menu);
+
+checkBox.addEventListener('change', changeTheme);
---
+++ goit-js-hw-10-food-service/src/components/theme.js
@@ -0,0 +1,28 @@
+const body = document.querySelector('body');
+const checkBox = document.querySelector('#theme-switch-control');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const currentTheme = localStorage.getItem('theme');
+
+if (JSON.parse(currentTheme) === Theme.DARK) {
+  checkBox.checked = true;
+  body.setAttribute('class', Theme.DARK);
+} else {
+  body.setAttribute('class', Theme.LIGHT);
+}
+
+const changeTheme = ({ target }) => {
+  if (target.checked) {
+    body.setAttribute('class', Theme.DARK);
+    localStorage.setItem('theme', JSON.stringify.Theme.DARK);
+  } else {
+    body.setAttribute('class', Theme.LIGHT);
+    localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));
+  }
+};
+
+export default changeTheme;
