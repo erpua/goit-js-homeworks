7f74c6e072edb455467dd93d56bb1c535b504c18
Yevgeniy Tovtin <zhe.tovtin@gmail.com>
2019-11-28 21:10:19 -0600

    changed switcher

--- goit-js-hw-10-food-service/src/components/handleSwitchingTheme.js
+++ goit-js-hw-10-food-service/src/components/handleSwitchingTheme.js
@@ -6,8 +6,8 @@ const Theme = {
 };
 
 const currentTheme = localStorage.getItem('theme') || Theme.LIGHT;
-
 refs.body.classList.add(currentTheme);
+if (localStorage.getItem('theme') === Theme.DARK) refs.switch.checked = true;
 
 function saveTheme(theme) {
   localStorage.setItem('theme', theme);
@@ -18,9 +18,6 @@ function handleSwitchingTheme() {
     refs.body.classList.remove(Theme.LIGHT);
     refs.body.classList.add(Theme.DARK);
     saveTheme(Theme.DARK);
-    if (localStorage.getItem('theme') === Theme.DARK) {
-      refs.switch.checked = true;
-    }
   } else {
     refs.body.classList.remove(Theme.DARK);
     refs.body.classList.add(Theme.LIGHT);
